fad74bfc1b8f0077f2a4ab9a30a7dc290e43b883
Kim Litjens <Kimlitjens4@gmail.com>
2021-07-10 21:10:10 +0200

    add basic styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import Square from './Square'
 
+const style = {
+    border: '4px solid darkblue',
+    borderRadius: '10px',
+    marginTop: '50px',
+    width: '250px',
+    height: '250px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+}
+
 const Board = ({ onClick, squares }) => (
-    <div>
+    <div style={style}>
         <Square value="1" onClick={() => onClick("something")} />
         <Square value="2" onClick={() => onClick("something")} />
         <Square value="3" onClick={() => onClick("something")} />
