e2f79f385260d3db1b839075563021175577e622
Kim Litjens <Kimlitjens4@gmail.com>
2021-07-10 21:01:46 +0200

    basic setup Board, Game & Square comp.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import Square from './Square'
 
-const Board = () => <div>Board</div>
+const Board = ({ onClick, squares }) => (
+    <div>
+        <Square value="1" onClick={() => onClick("something")} />
+        <Square value="2" onClick={() => onClick("something")} />
+        <Square value="3" onClick={() => onClick("something")} />
+        <Square value="4" onClick={() => onClick("something")} />
+        <Square value="5" onClick={() => onClick("something")} />
+        <Square value="6" onClick={() => onClick("something")} />
+        <Square value="7" onClick={() => onClick("something")} />
+        <Square value="8" onClick={() => onClick("something")} />
+        <Square value="9" onClick={() => onClick("something")} />
+    </div>
+)
 
 export default Board
