996306d4ac88e480d5193b836c2e242870e80c04
Kim Litjens <Kimlitjens4@gmail.com>
2021-07-13 19:32:37 +0200

    add time travel function

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,32 +8,47 @@ const styles = {
 }
 
 const Game = () => {
-    const [board, setBoard] = useState(Array(9).fill(null))
+    const [history, setHistory] = useState([Array(9).fill(null)])
+    const [stepNumber, setStepNumber] = useState(0)
     const [xIsNext, setXIsNext] = useState(true)
-    const winner = calculateWinner(board)
+    const winner = calculateWinner(history[stepNumber])
 
     const handleClick = i => {
-        const copyBoard = [...board]
-        if (winner || copyBoard[i]) return
+        const timeInHistory = history.slice(0, stepNumber + 1)
+        const current = timeInHistory[stepNumber]
+        const squares = [...current]
+        if (winner || squares[i]) return
 
-        copyBoard[i] = xIsNext ? 'X' : 'O'
-        setBoard(copyBoard)
+        squares[i] = xIsNext ? 'X' : 'O'
+        setHistory([...timeInHistory, squares])
+        setStepNumber(timeInHistory.length)
+
         setXIsNext(!xIsNext)
     }
 
-    const jumpTo = () => {
-
+    const jumpTo = (step) => {
+        setStepNumber(step)
+        setXIsNext(step % 2 === 0)
     }
 
     const renderMoves = () => (
-        <button onClick={() => setBoard(Array(9).fill(null))}>
-            Start Game
-        </button>
+        history.map((_step, move) => {
+            const destination = move ? `Go to move#${move}` : 'Go to start'
+            return (
+                <li key={move}>
+                    <button onClick={() => jumpTo(move)}>
+                        {destination}
+                    </button>
+                </li>
+
+            )
+        })
+
     )
 
     return (
         <>
-            <Board squares={board} onClick={handleClick} />
+            <Board squares={history[stepNumber]} onClick={handleClick} />
             <div style={styles}>
                 <p>
                     {winner ? 'Winner: ' + winner
